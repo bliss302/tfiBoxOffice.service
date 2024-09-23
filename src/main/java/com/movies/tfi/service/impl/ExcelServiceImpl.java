b39ac3f78e63ebee514ca6018cdc223d43d07946
RadhasyamNunna <radhasyamnunna@gmail.com>
2024-09-24 00:17:29 +0530

    connected with mongo atlas and added actor upload sheet

--- src/main/java/com/movies/tfi/service/impl/ExcelServiceImpl.java
+++ src/main/java/com/movies/tfi/service/impl/ExcelServiceImpl.java
@@ -2,7 +2,9 @@ package com.movies.tfi.service.impl;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.movies.tfi.entity.Actor;
+import com.movies.tfi.payload.ActorDto;
 import com.movies.tfi.repository.ActorRepository;
+import com.movies.tfi.service.ActorService;
 import com.movies.tfi.service.ExcelService;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -29,8 +31,11 @@ public class ExcelServiceImpl implements ExcelService {
     @Autowired
     ActorRepository actorRepository;
 
+    @Autowired
+    ActorService actorService;
+
     @Override
-    public void saveActorsFromExcel(String file) {
+    public List<ActorDto> saveActorsFromExcel(String file) {
         try(InputStream inputStream = new FileInputStream(file); XSSFWorkbook workbook = new XSSFWorkbook(inputStream) ) {
             Sheet sheet = workbook.getSheetAt(0);
             Iterator<Row> rows = sheet.iterator();
@@ -41,25 +46,28 @@ public class ExcelServiceImpl implements ExcelService {
             List<Actor> actors = new ArrayList<>();
 
             SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-            for(int i=1;i<sheet.getLastRowNum();i++){
+            for(int i=1;i<=sheet.getLastRowNum();i++){
                 Row row = sheet.getRow(i);
                 Actor actor = new Actor();
                 actor.setActorId((long)row.getCell(0).getNumericCellValue());
                 actor.setFirstName(row.getCell(1).getStringCellValue());
                 actor.setLastName(row.getCell(2).getStringCellValue());
                 actor.setCategory(row.getCell(3).getStringCellValue());
-                actor.setDateOfBirth( dateFormatter.parse(row.getCell(4).getStringCellValue()));
+                actor.setDateOfBirth( row.getCell(4).getDateCellValue());
 
                 actors.add(actor);
             }
 
-            actorRepository.saveAll(actors);
+            List<Actor> actorList = actorRepository.saveAll(actors);
             workbook.close();
             inputStream.close();
+            List<ActorDto> actorDtos = actorList.stream().map(element -> actorService.mapToDto(element)).toList();
+            return actorDtos;
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
         }
+//        catch (ParseException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
